bee96177babce7c53724fb2f743027fbbb6c448e
John <zz.john.cn@gmail.com>
2022-07-19 14:47:06 +0800

    修改：     for_go/main_test.go

--- for_go/main_test.go
+++ for_go/main_test.go
@@ -2,22 +2,12 @@ package main
 
 import "testing"
 
-func TestLoop_for_empty(t *testing.T) {
-	Loop_for_empty()
+func TestLoops(t *testing.T) {
+	Loops()
 }
 
-func TestLoop_for_add(t *testing.T) {
-	Loop_for_add()
-}
-
-func BenchmarkLoop_for_empty(b *testing.B) {
+func BenchmarkLoops(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Loop_for_empty()
-	}
-}
-
-func BenchmarkLoop_for_add(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Loop_for_add()
+		Loops()
 	}
 }
